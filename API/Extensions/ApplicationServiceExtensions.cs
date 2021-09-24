981f50e6d9be771c88ae43d119b55bce449de967
Michael Brady <michael.j.brady6@gmail.com>
2021-09-24 09:50:50 -0700

    End of Section 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            // AddScoped considered most appropriate for HttpRequests. Heavily used.
+            // Interface isn't technically needed but aids in testing later, allows for mockups. Best practice.
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DataContext>(options => 
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+        
+    }
+}
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        //Symmetric Encryption where one key is used to encrypt and decrypt information.
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            // Generate claim for our token (Payload/Data).
+            var claims = new List<Claim>
+            {                
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            // Create credentials for our token. Need a key and an algorithm.
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            // Describe our token (what goes inside).
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            // Create the token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}

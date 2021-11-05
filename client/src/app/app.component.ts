31abfef83a7127fad864440fb32def8713006d15
Michael Brady <michael.j.brady6@gmail.com>
2021-11-04 20:55:27 -0700

    Committing for kicks

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +15,23 @@ export class AppComponent implements OnInit {
   //"any" indicates that user can be any type. Turns of type safety.
   users: any;
 
-  constructor(private http: HttpClient) {}
+  // don't need HttpClient if not doing getUsers(). Commenting out this version of constructor
+  //constructor(private http: HttpClient, private accountService: AccountService) {}
+
+  constructor(private accountService: AccountService) {}
   
   ngOnInit(): void {
-    this.getUsers();
+    // No longer using but keeping for reference
+    //this.getUsers();
+    this.setCurrentUser();
   }
 
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
+  }
+
+  /*no longer using but keeping for reference
   getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe(response => {
       this.users = response;
@@ -26,4 +39,5 @@ export class AppComponent implements OnInit {
       console.log(error);
     })
   }
+  */
 }

365e9070dc202ae4d3da8b26262daa813180baa6
Michael Brady <michael.j.brady6@gmail.com>
2021-11-11 20:30:38 -0700

    End of section 5

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -8,19 +8,25 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  users: any;
+  //users: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(/*private http: HttpClient*/) { }
 
   ngOnInit(): void {
-    this.getUsers();
+    //this.getUsers();
   }
 
   registerToggle() {
     this.registerMode = ! this.registerMode;
   }
 
+  /*
   getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe(users => this.users = users);
+  }
+  */
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
   }
 }
